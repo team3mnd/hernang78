1dae82fd2d3fcb4a75009ad023b62dd5e6361be6
Orbe <oscar.orbe@accenture.com>
2019-11-07 11:51:23 -0300

    Update render de imagenes

--- src/componentes/Slide/Slide.js
+++ src/componentes/Slide/Slide.js
@@ -2,21 +2,31 @@ import React from 'react';
 import './Slide.css';
 import { images } from './images';
 import img1 from './1.jpg';
-import imgs from './imgs';
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+import img7 from "./7.jpg";
+import img8 from "./8.jpg";
+import img9 from "./9.jpg";
+import img10 from "./10.jpg";
+import img11 from "./11.jpg";
+import img12 from "./12.jpg";
 
 let backs = [
-  images.n1,
-  images.n2,
-  images.n3,
-  images.n4,
-  images.n5,
-  images.n6,
-  images.n7,
-  images.n8,
-  images.n9,
-  images.n10,
-  images.n11,
-  images.n12
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12
 ]
 
 export class Slide extends React.Component {
@@ -31,7 +41,7 @@ export class Slide extends React.Component {
 
   left() {
     const c = this.state.class;
-    const x = (this.state.backs.length - 1) / 4;
+    const x = (backs.length - 1) / 4;
     if (c > 1) {
       this.setState({ class: c - 1 })
     } else {
@@ -54,19 +64,16 @@ export class Slide extends React.Component {
   }
 
   render() {
-    const n1 = {backgroundImage: 'require(' + backs[this.state.class * 1] + ')'};
-    const n2 = {backgroundImage: 'require(' + backs[this.state.class * 2] + ')'};
-    const n3 = {backgroundImage: 'require(' + backs[this.state.class * 3] + ')'};
-    const n4 = {backgroundImage: 'require(' + backs[this.state.class * 4] + ')'};
+    const n1 = {backgroundImage: 'url(' + backs[(this.state.class * 1) - 1] + ')'};
+    const n2 = {backgroundImage: 'url(' + backs[(this.state.class * 2) - 1] + ')'};
+    const n3 = {backgroundImage: 'url(' + backs[(this.state.class * 3) - 1] + ')'};
+    const n4 = {backgroundImage: 'url(' + backs[(this.state.class * 4) - 1] + ')'};
     return <div>
       <button onClick={this.left}>Left</button>
       <div className="slide" style={ n1 }></div>
       <div className="slide" style={ n2 }></div>
       <div className="slide" style={ n3 }></div>
       <div className="slide" style={ n4 }></div>
-      <img src={ backs[0] } alt='' />
-      <img src={ imgs[0] } alt='' />
-      <img src={ images.n1 } alt='' />
       <button onClick={this.right}>Right</button>
     </div>
   }
