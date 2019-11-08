6387db89caae5a21f8aff933eb0a6e1d22a5e2a4
Orbe <oscar.orbe@accenture.com>
2019-11-08 16:57:49 -0300

    Arreglado el aspect ratio de las imágenes del carousel covereando todo el ancho y el alto del div contenedor

--- src/componentes/SlidePack/SlidePack.js
+++ src/componentes/SlidePack/SlidePack.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class SlidePack extends React.Component {
   fourImages() {
     return this.props.setimg.map((img, index) => {
-      return <div key={ index } className="overflow-hidden" style={{ width: "42vw", height: "30vw", margin: "1vw" }}><img className="col-xs-4 img-fluid d-block" style={{ width: "100%", height: "100%" }} src={img} alt={index} /></div>
+      return <div key={index} style={{ maxWidth: "40%", height: "30vw", margin: "1%", overflow: "hidden" }}><img style={{ height: "100%", width: "100%", objectFit: "cover" }} src={img} alt={index} /></div>
     })
   }
 
