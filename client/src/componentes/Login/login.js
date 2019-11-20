2c921425cca903b1b9942e83b32872b088c8fbd0
damianmartinez92 <damian.martinez@live.com.ar>
2019-11-20 14:39:25 -0300

    cambios varios de estetica en navbar y componentes, se agrego foto y acomodo un poco el css en general, se empezo a implementar carpetas de redux

--- client/src/componentes/Login/login.js
+++ client/src/componentes/Login/login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default class Login extends Component {
   render() {
     return (
       <div>
-        <p>aca te vas a loggear</p>
+        <p>Log In</p>
+        <label htmlFor="user">User: </label>
+        <input type="text"/>
+        <br/>
+        <label htmlFor="pass">Password: </label>
+        <input type="password"/>
+        <br/>
+        <Button>Log In</Button>
       </div>
     );
   }
--- client/src/componentes/Signup/signup.js
+++ client/src/componentes/Signup/signup.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
 
 export default class Signup extends Component {
   render() {
     return (
       <div>
-        <p>aca te vas a registrar</p>
+        <p>Create new account</p>
+        <label htmlFor="name">Name: </label>
+        <input type="text" />
+        <br />
+        <label htmlFor="user">User: </label>
+        <input type="text" />
+        <br />
+        <label htmlFor="pass">New password: </label>
+        <input type="password" />
+        <br />
+        <Button>Create New User</Button>
       </div>
     );
   }
