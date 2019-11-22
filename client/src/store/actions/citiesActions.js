744a2358e476f30a14286b59338e555168307cd8
damianmartinez92 <damian.martinez@live.com.ar>
2019-11-22 09:06:58 -0300

    implementacion de cities con redux

--- client/src/store/actions/citiesActions.js
+++ client/src/store/actions/citiesActions.js
@@ -1,19 +1,18 @@
-import {GET_CITIES} from '../constants'
+import { GET_CITIES } from "../constants";
 
-const fetchDataCities = (cities) => {
-    return {
-        // siempre el que importe
-        type: GET_CITIES,
-        cities
-    }
-}
+const fetchDataCities = cities => {
+  return {
+    // siempre el que importe
+    type: GET_CITIES,
+    cities
+  };
+};
 
-export const getAllCities = (dispatch) => {
-    fetch("/cities", {method: "GET"})
+export const getAllCities = () => async dispatch => {
+  await fetch("/cities", { method: "GET" })
     .then(response => response.json())
-    .then(cities => { 
-        dispatch(fetchDataCities(cities))
+    .then(cities => {
+      dispatch(fetchDataCities(cities));
     })
     .catch(err => console.log(err));
-}
-
+};
