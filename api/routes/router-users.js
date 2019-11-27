5fdd4170e43a83d21477c8a7ad0bedf61192bd57
emiliana <emiliana.ailen@hotmail.com>
2019-11-27 11:43:42 -0300

    express validator

--- api/routes/router-users.js
+++ api/routes/router-users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const { check, validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
   User.find()
@@ -12,28 +13,37 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/add', (req, res, next) => {
-  const user = new User({
-    userName: req.body.userName,
-    country: req.body.country,
-    password: req.body.password,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    img: req.body.picture,
-    mail: req.body.mail
+router.post('/add',
+  [
+    check('mail').isEmail(),
+    check('password').isLength({ min: 5 })
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    else {
+      const user = new User({
+        userName: req.body.userName,
+        country: req.body.country,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        img: req.body.picture,
+        mail: req.body.mail
+      })
+
+      user
+        .save(function (err) {
+          if (err) {
+            res.send(err.message);
+          }
+          else {
+            res.send('User added successfully')
+          }
+        });
+    }
   });
- 
-  user
-    .save(function (err) {
-      if (err) {
-        res.send(err.message);
-      }
-      else {
-        res.send('User added successfully')
-      }
-    });
-})
-
-
 
 module.exports = router;
