db791e54f8642e1f27bb4830bf9aeacda4a60cc1
hcignacio <46901747+hcignacio@users.noreply.github.com>
2019-11-27 11:26:37 -0300

    mongoose - useCreateIndex

--- api/models/user.js
+++ api/models/user.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
+/* mongoose.set('useCreateIndex', true); */
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    password: { type: String, required: true },
-    userName: {
-        type: String,
-        unique: true
-    },
-    mail: { type: String, unique: true, required: true },
-    country: String,
-    picture: String
+  firstName: String,
+  lastName: String,
+  password: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    unique: true
+  },
+  mail: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  country: String,
+  picture: String
 });
 
 userSchema.plugin(uniqueValidator);
