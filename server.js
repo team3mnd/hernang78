67c7412bfbef5c9c9dc6526536cb8044bbd30537
mauricio <mauriciogallegoavi@gmail.com>
2019-12-04 13:00:07 -0300

    agrgado comentarios backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require('./database');
 const cities = require('./api/routes/router-cities');
 const itineraries = require('./api/routes/router-itineraries');
 const users = require('./api/routes/router-users');
+const comments = require('./api/routes/router-comments.js');
 const passport = require('./passport');
 //passport middleware
 app.use(passport.initialize());
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/cities', cities);
 app.use('/itineraries', itineraries);
 app.use('/users',users);
+app.use('/comments',comments);
 app.use(express.static('images'));;
 
 app.listen(5000, function () {
