76f295cb339d81715ff3ac872520f029bdc5f100
emiliana <emiliana.ailen@hotmail.com>
2019-12-05 12:49:22 -0300

    imagenes de ciudades cambiadas

---
+++ client/src/componentes/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./Footer.css";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="FooterStyle bg-dark text-center">
+        <p className="mb-0 small">Mytinerary © - all rights reserved</p>
+      </div>
+    )
+  }
+}
